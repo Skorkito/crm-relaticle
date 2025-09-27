5c23af61cda6c2a635a52b727d141621fcc76a09
Tomasz Sasinowski <tomasz_sasinowski@wsip.com.pl>
2025-09-27 14:59:33 +0200

    _

--- src/app/Providers/Filament/AppPanelProvider.php
+++ src/app/Providers/Filament/AppPanelProvider.php
@@ -74,7 +74,7 @@ final class AppPanelProvider extends PanelProvider
             ->emailVerification()
             ->databaseNotifications()
             ->brandLogoHeight('2.6rem')
-            ->brandLogo(fn () => public_path('images/sorix_logo.svg'))
+            ->brandLogo(asset('images/sorix_logo.svg'))
             ->viteTheme('resources/css/app.css')
             ->colors([
                 'primary' => '#2072FB',
